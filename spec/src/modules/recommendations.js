59cf70abf1a997bfaf2aaae4066da437738e2803
Steve Blaurock <sblaurock@gmail.com>
2019-09-17 14:24:16 -0400

    Add tests to cover remaining methods in recommendations module.

--- spec/src/modules/recommendations.js
+++ spec/src/modules/recommendations.js
@@ -125,4 +125,282 @@ describe('ConstructorIO - Recommendations', () => {
         .notify(done);
     });
   });
+
+  describe('getComplementaryItems', () => {
+    const itemId = 'power_drill';
+    const itemIds = [itemId, 'drill'];
+
+    beforeEach(() => {
+      global.SEARCH_VERSION = 'cio-mocha';
+    });
+
+    afterEach(() => {
+      delete global.SEARCH_VERSION;
+    });
+
+    it('Should return a response with valid itemIds (singular)', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getComplementaryItems(itemId).then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.request.item_id).to.equal(itemId);
+        expect(res.response).to.have.property('results').to.be.an('array');
+        done();
+      });
+    });
+
+    it('Should return a response with valid itemIds (multiple)', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getComplementaryItems(itemIds).then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.request.item_id).to.deep.equal(itemIds);
+        expect(res.response).to.have.property('results').to.be.an('array');
+        done();
+      });
+    });
+
+    it('Should return a response with valid itemIds, and segments', (done) => {
+      const segments = 'segments';
+      const { recommendations } = new ConstructorIO({
+        apiKey: testApiKey,
+        segments,
+      });
+
+      recommendations.getComplementaryItems(itemId).then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        done();
+      });
+    });
+
+    it('Should return a response with valid itemIds, and results', (done) => {
+      const results = 2;
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getComplementaryItems(itemId, { results }).then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.request.num_results).to.equal(results);
+        done();
+      });
+    });
+
+    it('Should return a response with valid itemIds, with a result_id appended to each result', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getComplementaryItems(itemId).then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.response).to.have.property('results').to.be.an('array');
+        res.response.results.forEach((item) => {
+          expect(item).to.have.property('result_id').to.be.a('string').to.equal(res.result_id);
+        });
+        done();
+      });
+    });
+
+    it('Should throw an error when invalid itemIds are provided', () => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      expect(() => recommendations.getComplementaryItems({})).to.throw('itemIds is a required parameter of type string or array');
+    });
+
+    it('Should throw an error when no itemIds are provided', () => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      expect(() => recommendations.getComplementaryItems()).to.throw('itemIds is a required parameter of type string or array');
+    });
+
+    it('Should throw an error when invalid results parameter is provided', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      return expect(recommendations.getComplementaryItems(itemId, { results: 'abc' }))
+        .to.eventually.be.rejectedWith('BAD REQUEST')
+        .and.be.an.instanceOf(Error)
+        .notify(done);
+    });
+
+    it('Should throw an error when invalid apiKey is provided', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: 'fyzs7tfF8L161VoAXQ8u' });
+
+      return expect(recommendations.getComplementaryItems(itemId))
+        .to.eventually.be.rejectedWith('BAD REQUEST')
+        .and.be.an.instanceOf(Error)
+        .notify(done);
+    });
+  });
+
+  describe('getRecentlyViewedItems', () => {
+    beforeEach(() => {
+      global.SEARCH_VERSION = 'cio-mocha';
+    });
+
+    afterEach(() => {
+      delete global.SEARCH_VERSION;
+    });
+
+    it('Should return a response', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getRecentlyViewedItems().then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.response).to.have.property('results').to.be.an('array');
+        done();
+      });
+    });
+
+    it('Should return a response with valid segments', (done) => {
+      const segments = 'segments';
+      const { recommendations } = new ConstructorIO({
+        apiKey: testApiKey,
+        segments,
+      });
+
+      recommendations.getRecentlyViewedItems().then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        done();
+      });
+    });
+
+    it('Should return a response with valid results', (done) => {
+      const results = 2;
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getRecentlyViewedItems({ results }).then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.request.num_results).to.equal(results);
+        done();
+      });
+    });
+
+    it('Should return a response with a result_id appended to each result', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getRecentlyViewedItems().then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.response).to.have.property('results').to.be.an('array');
+        res.response.results.forEach((item) => {
+          expect(item).to.have.property('result_id').to.be.a('string').to.equal(res.result_id);
+        });
+        done();
+      });
+    });
+
+    it('Should throw an error when invalid results parameter is provided', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      return expect(recommendations.getRecentlyViewedItems({ results: 'abc' }))
+        .to.eventually.be.rejectedWith('BAD REQUEST')
+        .and.be.an.instanceOf(Error)
+        .notify(done);
+    });
+
+    it('Should throw an error when invalid apiKey is provided', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: 'fyzs7tfF8L161VoAXQ8u' });
+
+      return expect(recommendations.getRecentlyViewedItems())
+        .to.eventually.be.rejectedWith('BAD REQUEST')
+        .and.be.an.instanceOf(Error)
+        .notify(done);
+    });
+  });
+
+  describe('getUserFeaturedItems', () => {
+    beforeEach(() => {
+      global.SEARCH_VERSION = 'cio-mocha';
+    });
+
+    afterEach(() => {
+      delete global.SEARCH_VERSION;
+    });
+
+    it('Should return a response', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getUserFeaturedItems().then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.response).to.have.property('results').to.be.an('array');
+        done();
+      });
+    });
+
+    it('Should return a response with valid segments', (done) => {
+      const segments = 'segments';
+      const { recommendations } = new ConstructorIO({
+        apiKey: testApiKey,
+        segments,
+      });
+
+      recommendations.getUserFeaturedItems().then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        done();
+      });
+    });
+
+    it('Should return a response with valid results', (done) => {
+      const results = 2;
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getUserFeaturedItems({ results }).then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.request.num_results).to.equal(results);
+        done();
+      });
+    });
+
+    it('Should return a response with a result_id appended to each result', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      recommendations.getUserFeaturedItems().then((res) => {
+        expect(res).to.have.property('request').to.be.an('object');
+        expect(res).to.have.property('response').to.be.an('object');
+        expect(res).to.have.property('result_id').to.be.an('string');
+        expect(res.response).to.have.property('results').to.be.an('array');
+        res.response.results.forEach((item) => {
+          expect(item).to.have.property('result_id').to.be.a('string').to.equal(res.result_id);
+        });
+        done();
+      });
+    });
+
+    it('Should throw an error when invalid results parameter is provided', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: testApiKey });
+
+      return expect(recommendations.getUserFeaturedItems({ results: 'abc' }))
+        .to.eventually.be.rejectedWith('BAD REQUEST')
+        .and.be.an.instanceOf(Error)
+        .notify(done);
+    });
+
+    it('Should throw an error when invalid apiKey is provided', (done) => {
+      const { recommendations } = new ConstructorIO({ apiKey: 'fyzs7tfF8L161VoAXQ8u' });
+
+      return expect(recommendations.getUserFeaturedItems())
+        .to.eventually.be.rejectedWith('BAD REQUEST')
+        .and.be.an.instanceOf(Error)
+        .notify(done);
+    });
+  });
 });
