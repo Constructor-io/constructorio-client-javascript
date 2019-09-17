3348697d5346c21b75472ccaad7890fc51a23fe7
Steve Blaurock <sblaurock@gmail.com>
2019-09-17 13:20:12 -0400

    Utilize JSDoc to generate dynamic code documentation.

--- src/constructorio.js
+++ src/constructorio.js
@@ -6,7 +6,22 @@ const { search } = require('./modules/search');
 const { autocomplete } = require('./modules/autocomplete');
 const { recommendations } = require('./modules/recommendations');
 
+/**
+ * Class to instantiate the ConstructorIO client.
+ */
 class ConstructorIO {
+  /**
+   * @param {string} apiKey - Constructor.io API key
+   * @param {string} [serviceUrl='https://ac.cnstrc.com'] - API URL endpoint
+   * @param {string} [segments] - User segments
+   * @param {object} [testCells] - User test cells
+   * @param {string} [clientId] - Client ID, defaults to value supplied by 'constructorio-id'
+   * @param {string} [sessionId] - Session id, defaults to value supplied by 'constructorio-id'
+   * @property {object} [search] - Interface to search module used to make related API calls
+   * @property {object} [autocomplete] - Interface to autocomplete module
+   * @property {object} [recommendations] - Interface to recommendations module
+   * @returns {class}
+   */
   constructor(options = {}) {
     const {
       apiKey,
