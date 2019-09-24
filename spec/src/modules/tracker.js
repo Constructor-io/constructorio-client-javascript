ebbac98138c1756717861d16b0b5337d73eb27db
Steve Blaurock <sblaurock@gmail.com>
2019-09-24 14:33:19 -0600

    Provide URL creation helper method for autocomplete endpoints.

--- spec/src/modules/tracker.js
+++ spec/src/modules/tracker.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const testApiKey = process.env.TEST_API_KEY;
 
-describe('ConstructorIO - Tracker', () => {
+describe.only('ConstructorIO - Tracker', () => {
   jsdom({
     url: 'http://localhost',
   });
@@ -36,6 +36,70 @@ describe('ConstructorIO - Tracker', () => {
       const { tracker } = new ConstructorIO({ apiKey: 'fyzs7tfF8L161VoAXQ8u' });
 
       return expect(tracker.sendSessionStart())
+        .to.eventually.be.rejectedWith('BAD REQUEST')
+        .and.be.an.instanceOf(Error)
+        .notify(done);
+    });
+  });
+
+  describe('sendInputFocus', () => {
+    beforeEach(() => {
+      global.CLIENT_VERSION = 'cio-mocha';
+    });
+
+    afterEach(() => {
+      delete global.CLIENT_VERSION;
+    });
+
+    it('Should respond with a valid response', (done) => {
+      const { tracker } = new ConstructorIO({ apiKey: testApiKey });
+
+      tracker.sendInputFocus().then((res) => {
+        expect(res).to.equal(true);
+        done();
+      });
+    });
+
+    it('Should throw an error when invalid apiKey is provided', (done) => {
+      const { tracker } = new ConstructorIO({ apiKey: 'fyzs7tfF8L161VoAXQ8u' });
+
+      return expect(tracker.sendInputFocus())
+        .to.eventually.be.rejectedWith('BAD REQUEST')
+        .and.be.an.instanceOf(Error)
+        .notify(done);
+    });
+  });
+
+  describe('sendAutocompleteSelect', () => {
+    const name = 'Where The Wild Things Are';
+    const validParameters = {
+      tr: 'click',
+      autocompleteSection: 'Products',
+      resultId: '123-456-789',
+      originalQuery: 'books',
+    };
+
+    beforeEach(() => {
+      global.CLIENT_VERSION = 'cio-mocha';
+    });
+
+    afterEach(() => {
+      delete global.CLIENT_VERSION;
+    });
+
+    it('Should respond with a valid response when name and parameters are provided', (done) => {
+      const { tracker } = new ConstructorIO({ apiKey: testApiKey });
+
+      tracker.sendAutocompleteSelect(name, validParameters).then((res) => {
+        expect(res).to.equal(true);
+        done();
+      });
+    });
+
+    it('Should throw an error when invalid apiKey is provided', (done) => {
+      const { tracker } = new ConstructorIO({ apiKey: 'fyzs7tfF8L161VoAXQ8u' });
+
+      return expect(tracker.sendAutocompleteSelect(name, validParameters))
         .to.eventually.be.rejectedWith('BAD REQUEST')
         .and.be.an.instanceOf(Error)
         .notify(done);
--- src/modules/tracker.js
+++ src/modules/tracker.js
@@ -13,12 +13,13 @@ const { fetch } = fetchPonyfill({ Promise });
  * @returns {object}
  */
 export function tracker(options) {
-  // Create URL from supplied parameters
-  const createTrackingUrl = (action) => {
+  // Create behavior URL from supplied parameters
+  const createBehaviorUrl = (action) => {
     const { apiKey, version, serviceUrl, sessionId, clientId } = options;
     const queryParams = { c: version };
     const validActions = [
       'session_start',
+      'focus',
     ];
 
     // Ensure supplied action is valid
@@ -35,6 +36,63 @@ export function tracker(options) {
     return `${serviceUrl}/behavior?${queryString}`;
   };
 
+  // Create autocomplete URL from supplied parameters
+  const createAutocompleteUrl = (action, name, parameters) => {
+    const { apiKey, version, serviceUrl, sessionId, clientId } = options;
+    const queryParams = { c: version };
+    const validActions = [
+      'select',
+      'search',
+    ];
+
+    // Ensure supplied action is valid
+    if (!action || validActions.indexOf(action) === -1) {
+      throw new Error(`action is a required parameter and must be one of the following strings: ${validActions.join(', ')}`);
+    }
+
+    // Validate product name is provided
+    if (!name || typeof name !== 'string') {
+      throw new Error('name is a required parameter of type string');
+    }
+
+    // Validate parameters are supplied and valid
+    if (!parameters || typeof parameters !== 'object' || !parameters.originalQuery || !parameters.resultId) {
+      throw new Error('parameters is a required object, as are parameters.originalQuery and parameters.resultId');
+    }
+
+    queryParams.key = apiKey;
+    queryParams.i = clientId;
+    queryParams.s = sessionId;
+
+    if (parameters) {
+      const { originalQuery, tr, autocompleteSection, resultId } = parameters;
+
+      // Pull original query from parameters
+      if (originalQuery) {
+        queryParams.original_query = originalQuery;
+      }
+
+      // Pull trigger (tr) from parameters
+      if (tr) {
+        queryParams.tr = tr;
+      }
+
+      // Pull autocomplete section from parameters
+      if (autocompleteSection) {
+        queryParams.autocomplete_section = autocompleteSection;
+      }
+
+      // Pull result id from parameters
+      if (resultId) {
+        queryParams.result_id = resultId;
+      }
+    }
+
+    const queryString = qs.stringify(queryParams, { indices: false });
+
+    return `${serviceUrl}/autocomplete/${encodeURIComponent(name)}/${action}?${queryString}`;
+  };
+
   return {
     /**
      * Send session start event to API
@@ -43,7 +101,7 @@ export function tracker(options) {
      * @returns {Promise}
      */
     sendSessionStart: () => {
-      const requestUrl = createTrackingUrl('session_start');
+      const requestUrl = createBehaviorUrl('session_start');
 
       return fetch(requestUrl).then((response) => {
         if (response.ok) {
@@ -54,8 +112,44 @@ export function tracker(options) {
       });
     },
 
-    sendAutocompleteSelect: () => {
+    /**
+     * Send input focus event to API
+     *
+     * @function sendInputFocus
+     * @returns {Promise}
+     */
+    sendInputFocus: () => {
+      const requestUrl = createBehaviorUrl('focus');
 
+      return fetch(requestUrl).then((response) => {
+        if (response.ok) {
+          return true;
+        }
+
+        throw new Error(response.statusText);
+      });
+    },
+
+    /**
+     * Send autocomplete select event to API
+     *
+     * @function sendAutocompleteSelect
+     * @param {string} name - Name of selected product
+     * @param {object} [parameters] - Additional parameters to be sent with request
+     * @param {number} [parameters.page] - The page number of the results
+     * @param {number} [parameters.resultsPerPage] - The number of results per page to return
+     * @returns {Promise}
+     */
+    sendAutocompleteSelect: (name, parameters) => {
+      const requestUrl = createAutocompleteUrl('select', name, parameters);
+
+      return fetch(requestUrl).then((response) => {
+        if (response.ok) {
+          return true;
+        }
+
+        throw new Error(response.statusText);
+      });
     },
 
     sendAutocompleteSearch: () => {
