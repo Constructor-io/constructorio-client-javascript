98aa0f6fb07bd1ffd6b5c06d4a7536a5f53635e6
Steve Blaurock <sblaurock@gmail.com>
2019-10-08 11:46:25 -0600

    Allow fetch to be passed in as an option.

--- src/modules/autocomplete.js
+++ src/modules/autocomplete.js
@@ -4,8 +4,6 @@ const fetchPonyfill = require('fetch-ponyfill');
 const Promise = require('es6-promise');
 const { throwHttpErrorFromResponse } = require('../utils');
 
-const { fetch } = fetchPonyfill({ Promise });
-
 /**
  * Interface to autocomplete related API calls.
  *
@@ -14,6 +12,8 @@ const { fetch } = fetchPonyfill({ Promise });
  * @returns {object}
  */
 const autocomplete = (options) => {
+  const fetch = (options && options.fetch) || fetchPonyfill({ Promise }).fetch;
+
   // Create URL from supplied query (term) and parameters
   const createAutocompleteUrl = (query, parameters) => {
     const { apiKey, version, serviceUrl, sessionId, clientId, segments, testCells } = options;
