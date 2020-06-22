9f87967d39510fbea0283968e88dffe5581e4b6d
Steve Blaurock <sblaurock@gmail.com>
2020-06-22 12:31:07 -0600

    Remove 'only'.

--- spec/src/utils/event-dispatcher.js
+++ spec/src/utils/event-dispatcher.js
@@ -9,7 +9,7 @@ const helpers = require('../../mocha.helpers');
 chai.use(chaiAsPromised);
 dotenv.config();
 
-describe.only('ConstructorIO - Utils - Event Dispatcher', () => {
+describe('ConstructorIO - Utils - Event Dispatcher', () => {
   const beaconEventName = 'cio.beacon.loaded';
   const eventData = {
     module: 'search',
