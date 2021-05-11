10a2fa41e2bb2488de3c93f8df4f7a1ffb5168fb
Jimmy Li <jimmyli921@gmail.com>
2021-05-11 13:03:30 -0700

    Update to use encodeURIComponenent in addition to qs.parse and stringify

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8,7 +8,7 @@ const purchaseEventStorageKey = '_constructorio_purchase_order_ids';
 const utils = {
   ourEncodeURIComponent: (str) => {
     if (str) {
-      const parsedStrObj = qs.parse(`s=${str.replace(/&/g, '%26')}`);
+      const parsedStrObj = qs.parse(`s=${encodeURIComponent(str)}`);
 
       parsedStrObj.s = parsedStrObj.s.replace(/\s/g, ' ');
 
