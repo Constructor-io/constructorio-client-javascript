29638076c51e07bae3cf31e34ca278cd4a222478
Stanley Peng <stanley.peng@constructor.io>
2022-01-04 12:08:55 -0800

    Initial quizzes implementation/debugging

---
+++ src/modules/quizzes.js
@@ -0,0 +1,181 @@
+/* eslint-disable object-curly-newline, no-underscore-dangle */
+const qs = require('qs');
+const fetchPonyfill = require('fetch-ponyfill');
+const Promise = require('es6-promise');
+const EventDispatcher = require('../utils/event-dispatcher');
+const helpers = require('../utils/helpers');
+
+// Create URL from supplied query (term) and parameters
+function createQuizUrl(quizId, parameters, options, path) {
+  const {
+    apiKey,
+  } = options;
+  const serviceUrl = 'https://quizzes.cnstrc.com/api';
+  let queryParams = { };
+
+  queryParams.index_key = apiKey;
+
+  // Validate quiz id is provided
+  if (!quizId || typeof quizId !== 'string') {
+    throw new Error('quizId is a required parameter of type string');
+  }
+
+  if (parameters) {
+    const { section, a, versionId } = parameters;
+
+    // Pull section from parameters
+    if (section) {
+      queryParams.section = section;
+    }
+
+    // Pull a (answers) from parameters
+    if (a) {
+      queryParams.a = a;
+    }
+
+    // Pull version_id from parameters
+    if (versionId) {
+      queryParams.version_id = versionId;
+    }
+  }
+
+  queryParams._dt = Date.now();
+  queryParams = helpers.cleanParams(queryParams);
+
+  const queryString = qs.stringify(queryParams, { indices: false });
+
+  return `${serviceUrl}/v1/quizzes/${encodeURIComponent(quizId)}/${encodeURIComponent(path)}/?${queryString}`;
+}
+
+/**
+ * Interface to quiz related API calls
+ *
+ * @module quizzes
+ * @inner
+ * @returns {object}
+ */
+class Quizzes {
+  constructor(options) {
+    this.options = options || {};
+    this.eventDispatcher = new EventDispatcher(options.eventDispatcher);
+  }
+
+  /**
+   * Retrieve next quiz from api
+   *
+   * @function getNextQuiz
+   * @description Retrieve search results from Constructor.io API
+   * @param {string} id - The id of the quiz
+   * @param {string} [parameters] - Additional parameters to refine result set
+   * @param {string} [parameters.index_key] - Index key for the customer's product catalog
+   * @param {string} [parameters.section] - Section for customer's product catalog (optional)
+   * @param {string} [parameters.a] - A list of answers in the format ?a=<option_id>,<option_id>&a=<option_id>
+   * @param {string} [version_id] - Specific version id for the quiz.
+   * @param {object} [networkParameters] - Parameters relevant to the network request
+   * @param {number} [networkParameters.timeout] - Request timeout (in milliseconds)
+   * @returns {Promise}
+   * @see https://quizzes.cnstrc.com/api/#/quizzes/QuizzesController_getQuizResult
+   * @example
+   * constructorio.search.getSearchResults('t-shirt', {
+   *     resultsPerPage: 40,
+   *     filters: {
+   *         size: 'medium'
+   *     },
+   * });
+   */
+  getNextQuiz(quizId, parameters, networkParameters = {}) {
+    console.log('called');
+    let requestUrl;
+    const fetch = (this.options && this.options.fetch) || fetchPonyfill({ Promise }).fetch;
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    try {
+      requestUrl = createQuizUrl(quizId, parameters, this.options, 'next');
+      console.log(requestUrl);
+    } catch (e) {
+      console.log(e);
+      return Promise.reject(e);
+    }
+
+    // Handle network timeout if specified
+    helpers.applyNetworkTimeout(this.options, networkParameters, controller);
+    console.log('here');
+    return fetch(requestUrl, { signal })
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
+        }
+        return helpers.throwHttpErrorFromResponse(new Error(), response);
+      })
+      .then((json) => {
+        // Search results
+        if (json.response && json.response.results) {
+          this.eventDispatcher.queue('quizzes.getNextQuiz.completed', json);
+          return json;
+        }
+
+        throw new Error('getNextQuiz response data is malformed');
+      });
+  }
+
+  /**
+   * Retrieves filter expression and recommendation URL from given answers.
+   *
+   * @function getFinalizeQuiz
+   * @description Retrieve search results from Constructor.io API
+   * @param {string} id - The id of the quiz
+   * @param {string} [parameters] - Additional parameters to refine result set
+   * @param {string} [parameters.index_key] - Index key for the customer's product catalog
+   * @param {string} [parameters.section] - Section for customer's product catalog (optional)
+   * @param {string} [parameters.a] - A list of answers in the format ?a=<option_id>,<option_id>&a=<option_id>
+   * @param {string} [version_id] - Specific version id for the quiz.
+   * @param {object} [networkParameters] - Parameters relevant to the network request
+   * @param {number} [networkParameters.timeout] - Request timeout (in milliseconds)
+   * @returns {Promise}
+   * @see https://quizzes.cnstrc.com/api/#/quizzes/QuizzesController_getQuizResult
+   * @example
+   * constructorio.search.getSearchResults('t-shirt', {
+   *     resultsPerPage: 40,
+   *     filters: {
+   *         size: 'medium'
+   *     },
+   * });
+   */
+  getFinalizeQuiz(quizId, parameters, networkParameters = {}) {
+    let requestUrl;
+    const fetch = (this.options && this.options.fetch) || fetchPonyfill({ Promise }).fetch;
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    try {
+      requestUrl = createQuizUrl(quizId, parameters, this.options, 'finalize');
+    } catch (e) {
+      return Promise.reject(e);
+    }
+
+    // Handle network timeout if specified
+    helpers.applyNetworkTimeout(this.options, networkParameters, controller);
+
+    return fetch(requestUrl, { signal })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return helpers.throwHttpErrorFromResponse(new Error(), response);
+      })
+      .then((json) => {
+        // Search results
+        if (json.response && json.response.results) {
+          this.eventDispatcher.queue('quizzes.getFinalizeQuiz.completed', json);
+          return json;
+        }
+
+        throw new Error('getFinalizeQuiz response data is malformed');
+      });
+  }
+}
+
+module.exports = Quizzes;
