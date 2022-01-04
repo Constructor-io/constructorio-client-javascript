0d2c6f857aa2a0ac27043f02ac93cb3f1a4dddd2
Stanley Peng <stanley.peng@constructor.io>
2022-01-04 12:25:30 -0800

    Fixed test bug

---
+++ spec/src/modules/quizzes.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-unused-expressions, import/no-unresolved */
+const jsdom = require('mocha-jsdom');
+const dotenv = require('dotenv');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const fetchPonyfill = require('fetch-ponyfill');
+const Promise = require('es6-promise');
+const fs = require('fs');
+const helpers = require('../../mocha.helpers');
+let ConstructorIO = require('../../../test/constructorio'); // eslint-disable-line import/extensions
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+dotenv.config();
+
+const { fetch } = fetchPonyfill({ Promise });
+const testApiKey = process.env.TEST_API_KEY;
+const clientVersion = 'cio-mocha';
+const bundled = process.env.BUNDLED === 'true';
+const bundledDescriptionSuffix = bundled ? ' - Bundled' : '';
+const timeoutRejectionMessage = bundled ? 'Aborted' : 'The user aborted a request.';
+
+describe(`ConstructorIO - Quizzes${bundledDescriptionSuffix}`, () => {
+  const jsdomOptions = { url: 'http://localhost' };
+  let fetchSpy;
+
+  if (bundled) {
+    jsdomOptions.src = fs.readFileSync(`./dist/constructorio-client-javascript-${process.env.PACKAGE_VERSION}.js`, 'utf-8');
+  }
+
+  jsdom(jsdomOptions);
+
+  beforeEach(() => {
+    global.CLIENT_VERSION = clientVersion;
+    window.CLIENT_VERSION = clientVersion;
+    fetchSpy = sinon.spy(fetch);
+
+    if (bundled) {
+      ConstructorIO = window.ConstructorioClient;
+    }
+  });
+
+  afterEach(() => {
+    delete global.CLIENT_VERSION;
+    delete window.CLIENT_VERSION;
+
+    fetchSpy = null;
+  });
+
+  describe.only('getNextQuiz', () => {
+    const quizId = '';
+    it('Should be rejected if an Invalid quizId is provided', () => {
+      const { quizzes } = new ConstructorIO({
+        apiKey: testApiKey,
+        fetch: fetchSpy,
+      });
+      return expect(quizzes.getNextQuiz(quizId, {})).to.eventually.be.rejected;
+    });
+  });
+});
