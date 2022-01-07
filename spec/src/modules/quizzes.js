524ab3d412948deb5ee77abc278a6364b4cdf218
Stanley Peng <stanley.peng@constructor.io>
2022-01-07 08:50:43 -0800

    Add more quizzes, parse answers parameter

--- spec/src/modules/quizzes.js
+++ spec/src/modules/quizzes.js
@@ -17,12 +17,14 @@ dotenv.config();
 
 const { fetch } = fetchPonyfill({ Promise });
 const testApiKey = process.env.TEST_API_KEY;
+const quizApiKey = process.env.QUIZ_API_KEY;
 const clientVersion = 'cio-mocha';
 const bundled = process.env.BUNDLED === 'true';
 const bundledDescriptionSuffix = bundled ? ' - Bundled' : '';
 const timeoutRejectionMessage = bundled ? 'Aborted' : 'The user aborted a request.';
 
 describe(`ConstructorIO - Quizzes${bundledDescriptionSuffix}`, () => {
+  const validQuizId = 'etchells-emporium-quiz';
   const jsdomOptions = { url: 'http://localhost' };
   let fetchSpy;
 
@@ -50,13 +52,66 @@ describe(`ConstructorIO - Quizzes${bundledDescriptionSuffix}`, () => {
   });
 
   describe.only('getNextQuiz', () => {
-    const quizId = '';
-    it('Should be rejected if an Invalid quizId is provided', () => {
+
+    it('Should be rejected if an invalid quizId is provided', () => {
       const { quizzes } = new ConstructorIO({
-        apiKey: testApiKey,
+        apiKey: quizApiKey,
         fetch: fetchSpy,
       });
-      return expect(quizzes.getNextQuiz(quizId, {})).to.eventually.be.rejected;
+      return expect(quizzes.getNextQuiz('notaquizid', {})).to.eventually.be.rejected;
     });
+
+    it('Should be rejected if an invalid index_key/apiKey is provided', () => {
+      const { quizzes } = new ConstructorIO({
+        apiKey: 'invalidKey',
+        fetch: fetchSpy,
+      });
+      return expect(quizzes.getNextQuiz(validQuizId, {})).to.eventually.be.rejected;
+    });
+
+    it('Should return a result provided a valid index_key and quizId', () => {
+      const { quizzes } = new ConstructorIO({
+        apiKey: quizApiKey,
+        fetch: fetchSpy,
+      });
+      return quizzes.getNextQuiz(validQuizId, {}).then((res) => {
+        expect(res).to.have.property('version_id').to.be.an('string');
+        expect(res.next_question.id).to.equal(1);
+        expect(res.next_question.options[0].id).to.equal(1);
+      });
+    });
+
+    it('Should return result given answers parameter', () => {
+      const { quizzes } = new ConstructorIO({
+        apiKey: quizApiKey,
+        fetch: fetchSpy,
+      });
+
+      return quizzes.getNextQuiz(validQuizId, { a: [[1, 2, 3], [1]] }).then((res) => {
+        expect(res).to.have.property('version_id').to.be.an('string');
+        expect(res.next_question.id).to.equal(3);
+        expect(res.next_question.options[0].id).to.equal(1);
+      });
+    });
+
+    it('Should be rejected when network request timeout is provided and reached', () => {
+      const { quizzes } = new ConstructorIO({
+        apiKey: quizApiKey,
+        fetch: fetchSpy,
+      });
+
+      return expect(quizzes.getNextQuiz(validQuizId, {}, { timeout: 20 })).to.eventually.be.rejectedWith('The user aborted a request.');
+    });
+
+    it('Should be rejected when global network request timeout is provided and reached', () => {
+      const { quizzes } = new ConstructorIO({
+        apiKey: quizApiKey,
+        fetch: fetchSpy,
+        networkParameters: { timeout: 20 },
+      });
+
+      return expect(quizzes.getNextQuiz(validQuizId, {})).to.eventually.be.rejectedWith('The user aborted a request.');
+    });
+
   });
 });
