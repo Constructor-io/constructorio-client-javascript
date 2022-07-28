9072765faef43643bf77340e21c1e25b285d0c54
Stanley Peng <speng1011@gmail.com>
2022-07-28 11:54:34 -0700

    Lint fixes?

--- spec/src/modules/browse.js
+++ spec/src/modules/browse.js
@@ -862,7 +862,7 @@ describe(`ConstructorIO - Browse${bundledDescriptionSuffix}`, () => {
         expect(res).to.have.property('result_id').to.be.an('string');
         expect(res.request.fmt_options.hidden_facets).to.eql(hiddenFacets);
         expect(requestedUrlParams.fmt_options).to.have.property('hidden_facets').to.eql(hiddenFacets);
-        expect(res.response.facets.find((e) => e.name === 'Brand')).to.have.property('name').to.eql('Brand');
+        expect(res.response.facets.find(e => e.name === 'Brand')).to.have.property('name').to.eql('Brand');
         done();
       });
     });
