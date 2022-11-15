984682bc489ab03714c745796572a3531119ba03
mocca102 <islaam.agamy@gmail.com>
2022-11-15 12:35:44 +0200

    remove reference to customers

--- src/types/tests/browse.test-d.ts
+++ src/types/tests/browse.test-d.ts
@@ -1,7 +1,6 @@
 import { expectType } from "tsd";
-import { GetBrowseResultsResponse, GetBrowseGroupsResponse, GetBrowseFacetsResponse, GetBrowseFacetOptionsResponse } from "../browse";
+import { GetBrowseResultsResponse } from "../browse";
 
-// Example response from https://www.petflow.com/explorer?group_id=33149&f%5B%5D=pet:dog&limit=24&type=list&f%5B%5D=type:flea_%26_tick&f%5B%5D=pet:dog_%26_cat
 expectType<GetBrowseResultsResponse>({
 	response: {
 		result_sources: {
@@ -20,7 +19,7 @@ expectType<GetBrowseResultsResponse>({
 				options: [
 					{
 						status: "selected",
-						count: 17,
+						count: 12,
 						display_name: "dog",
 						value: "dog",
 						data: {},
@@ -34,45 +33,26 @@ expectType<GetBrowseResultsResponse>({
 			{
 				matched_terms: [],
 				data: {
-					id: "47771",
-					url: "https://www.petflow.com/product/bravecto/bravecto-soft-chews-fluralaner-for-large-breed-dogs?size=55940",
+					id: "123",
+					url: "https://example",
 					brand: "Bravecto",
 					price: 21.99,
-					item_name: "bravecto soft chews fluralaner for large breed dogs",
+					item_name: "Name",
 					image_url:
-						"https://www.petflow.com/images/default/products/maximal/47771-1631032159.jpg",
-					purchasable: 1,
-					order_limit: 0,
-					deactivated: false,
-					max_per_order: 0,
-					review_quality: 0,
-					review_quantity: 0,
-					default_option_id: 55940,
-					upc: 8713184199225,
-					msrp: 34.99,
+						"https://example",
 					published: true,
-					brand_slug: "bravecto",
-					variation_id: "55932",
-					size_description: "1-Month Supply",
+					variation_id: "123",
 				},
-				value: "Bravecto Soft Chews Fluralaner for Large Breed Dogs",
+				value: "Name",
 				is_slotted: false,
 				labels: {},
 				variations: [
 					{
 						data: {
-							upc: 8713184199225,
-							msrp: 34.99,
-							price: 21.99,
-							published: true,
-							brand_slug: "bravecto",
-							purchasable: 1,
-							deactivated: false,
-							variation_id: "55932",
-							max_per_order: 0,
-							size_description: "1-Month Supply",
+							brand_slug: "123",
+							variation_id: "123",
 						},
-						value: "Bravecto Soft Chews Fluralaner for Large Breed Dogs",
+						value: "123",
 					},
 				],
 			},
@@ -110,7 +90,7 @@ expectType<GetBrowseResultsResponse>({
 		sort_by: "relevance",
 		sort_order: "descending",
 		browse_filter_name: "group_id",
-		browse_filter_value: "33149",
+		browse_filter_value: "123",
 		term: "",
 		fmt_options: {
 			groups_start: "current",
@@ -138,376 +118,4 @@ expectType<GetBrowseResultsResponse>({
 		searchandized_items: {},
 	},
 	ad_based: true,
-});
-
-// Example response from https://www.everydayyoga.com/collections/mens-yoga-shirts-2405
-expectType<GetBrowseResultsResponse>({
-	response: {
-		result_sources: {
-			token_match: {
-				count: 49,
-			},
-			embeddings_match: {
-				count: 0,
-			},
-		},
-		facets: [
-			{
-				display_name: "SizeList",
-				name: "SizeList",
-				type: "multiple",
-				options: [
-					{
-						status: "",
-						count: 37,
-						display_name: "XL",
-						value: "XL",
-						data: {},
-					},
-					{
-						status: "",
-						count: 32,
-						display_name: "L",
-						value: "L",
-						data: {},
-					},
-					{
-						status: "",
-						count: 39,
-						display_name: "M",
-						value: "M",
-						data: {},
-					},
-					{
-						status: "",
-						count: 32,
-						display_name: "S",
-						value: "S",
-						data: {},
-					},
-					{
-						status: "",
-						count: 3,
-						display_name: "XXL",
-						value: "XXL",
-						data: {},
-					},
-					{
-						status: "",
-						count: 2,
-						display_name: "XS",
-						value: "XS",
-						data: {},
-					},
-				],
-				hidden: false,
-				data: {},
-			},
-		],
-		groups: [
-			{
-				group_id: "All",
-				display_name: "All",
-				count: 49,
-				data: {},
-				children: [
-					{
-						group_id: "87547117603",
-						display_name: "CAT:Yoga Shop|yoga-shop|2384|Y",
-						count: 49,
-						data: {},
-						children: [
-							{
-								group_id: "87551442979",
-								display_name: "CAT:Yoga Clothing |yoga-clothes|2385|Y",
-								count: 49,
-								data: {},
-								children: [],
-								parents: [
-									{
-										display_name: "All",
-										group_id: "All",
-									},
-									{
-										display_name: "CAT:Yoga Shop|yoga-shop|2384|Y",
-										group_id: "87547117603",
-									},
-								],
-							},
-						],
-						parents: [
-							{
-								display_name: "All",
-								group_id: "All",
-							},
-						],
-					},
-				],
-				parents: [],
-			},
-		],
-		results: [
-			{
-				matched_terms: [],
-				data: {
-					id: "8139734",
-					url: "https://www.everydayyoga.com/products/yak-yeti-mens-bohemian-long-sleeve-8139734",
-					group_ids: ["87552491555"],
-					VideoSize: 0,
-					ListPrice: 27,
-					NextColor: "Blue|210|14826148921379|1740075925539",
-					image_url:
-						"https://cdn.shopify.com/s/files/1/0032/7539/1011/products/1740075696163-2t.jpg?v=1636444021",
-					AvgRating: 4.6,
-					SalePrice: 0,
-					PriceRange: "",
-					IsMapPrice: "N",
-					IsFinalSale: false,
-					description:
-						"Use this Bohemian Long Sleeve top after your yoga session or wear with your favorite pair of jeans for a laid-back look. Features    Long sleeve pullover short. All over stripped pattern. Button-up neckline with collar.   Details    Fabric: 100% Cott",
-					ColorIdlist: "_Green_$_209_,_Blue_$_210_,",
-					productCode: 8139734,
-					MemberPrice: "",
-					TotalReviews: 10,
-					facets: [
-						{
-							name: "IsBestSeller",
-							values: ["True"],
-						},
-					],
-					NextColorSale: "Green|209|14826149773347|1740076285987",
-					ShopifyImgList:
-						"1740075925539-blue-1a.jpg?v=1636444023;1740075925539-blue-2a.jpg?v=1636444026;1740075925539-blue-3a.jpg?v=1636444028;1740075925539-blue-4a.jpg?v=1636444030;1740075925539-blue-5a.jpg?v=1636444032;1740075925539-swatch-blue.jpg?v=1636444034;1740075925539-blue.jpg?v=1636444036;1740076285987-green-1a.jpg?v=1636444039;1740076285987-green-2a.jpg?v=1636444041;1740076285987-green-3a.jpg?v=1636444043;1740076285987-green-4a.jpg?v=1636444046;1740076285987-green-5a.jpg?v=1636444048;1740076285987-swatch-green.jpg?v=1636444050;1740076285987-green.jpg?v=1636444052",
-					ColorSwatchList: "*$Green*$_*$209*$_*$1*$,*$Blue*$_*$210*$_*$1*$",
-					HasVideoCaption: "N",
-					DisplaySalePrice: 0,
-					ShopifyProductId: 1740075696163,
-					AdditionalMessage: "",
-					ColorSwatchListSale: "*$Green*$_*$209*$_*$1*$,*$Blue*$_*$210*$_*$1*$",
-				},
-				value: "Yak & Yeti Men's Bohemian Long Sleeve",
-				is_slotted: false,
-				labels: {},
-			},
-		],
-		sort_options: [
-			{
-				sort_by: "relevance",
-				display_name: "Featured",
-				sort_order: "descending",
-				status: "selected",
-			},
-		],
-		refined_content: [],
-		total_num_results: 49,
-		features: [
-			{
-				feature_name: "auto_generated_refined_query_rules",
-				display_name: "Affinity Engine",
-				enabled: true,
-				variant: {
-					name: "default_rules",
-					display_name: "Default weights",
-				},
-			},
-		],
-	},
-	result_id: "6a42ecb5-4852-4267-bdda-b5d8f3085da4",
-	request: {
-		page: 1,
-		num_results_per_page: 48,
-		fmt_options: {
-			groups_start: "top",
-			groups_max_depth: 6,
-		},
-		browse_filter_name: "group_id",
-		browse_filter_value: "87552491555",
-		term: "",
-		sort_by: "relevance",
-		sort_order: "descending",
-		section: "Products",
-		features: {
-			query_items: true,
-			auto_generated_refined_query_rules: true,
-			manual_searchandizing: true,
-			personalization: true,
-			filter_items: true,
-		},
-		feature_variants: {
-			query_items: "query_items_ctr_and_l2r",
-			auto_generated_refined_query_rules: "default_rules",
-			manual_searchandizing: null,
-			personalization: "default_personalization",
-			filter_items: "filter_items_w_atcs_and_purchases",
-		},
-		searchandized_items: {},
-	},
-	ad_based: true,
-});
-
-expectType<GetBrowseGroupsResponse>({
-	response: {
-		result_sources: {
-		},
-		groups: [
-			{
-				group_id: "All",
-				display_name: "All",
-				count: 49,
-				data: {},
-				children: [
-					{
-						group_id: "87547117603",
-						display_name: "CAT:Yoga Shop|yoga-shop|2384|Y",
-						count: 49,
-						data: {},
-						children: [
-							{
-								group_id: "87551442979",
-								display_name: "CAT:Yoga Clothing |yoga-clothes|2385|Y",
-								count: 49,
-								data: {},
-								children: [],
-								parents: [
-									{
-										display_name: "All",
-										group_id: "All",
-									},
-									{
-										display_name: "CAT:Yoga Shop|yoga-shop|2384|Y",
-										group_id: "87547117603",
-									},
-								],
-							},
-						],
-						parents: [
-							{
-								display_name: "All",
-								group_id: "All",
-							},
-						],
-					},
-				],
-				parents: [],
-			},
-		],
-	},
-});
-
-expectType<GetBrowseFacetsResponse>({
-	response: {
-		facets: [
-			{
-				display_name: "SizeList",
-				name: "SizeList",
-				type: "multiple",
-				options: [
-					{
-						status: "",
-						count: 37,
-						display_name: "XL",
-						value: "XL",
-						data: {},
-					},
-					{
-						status: "",
-						count: 32,
-						display_name: "L",
-						value: "L",
-						data: {},
-					},
-					{
-						status: "",
-						count: 39,
-						display_name: "M",
-						value: "M",
-						data: {},
-					},
-					{
-						status: "",
-						count: 32,
-						display_name: "S",
-						value: "S",
-						data: {},
-					},
-					{
-						status: "",
-						count: 3,
-						display_name: "XXL",
-						value: "XXL",
-						data: {},
-					},
-					{
-						status: "",
-						count: 2,
-						display_name: "XS",
-						value: "XS",
-						data: {},
-					},
-				],
-				hidden: false,
-				data: {},
-			},
-		],
-		total_num_results: 4
-	},
-});
-
-
-expectType<GetBrowseFacetOptionsResponse>({
-	response: {
-		facets: [
-			{
-				display_name: "SizeList",
-				name: "SizeList",
-				type: "multiple",
-				options: [
-					{
-						status: "",
-						count: 37,
-						display_name: "XL",
-						value: "XL",
-						data: {},
-					},
-					{
-						status: "",
-						count: 32,
-						display_name: "L",
-						value: "L",
-						data: {},
-					},
-					{
-						status: "",
-						count: 39,
-						display_name: "M",
-						value: "M",
-						data: {},
-					},
-					{
-						status: "",
-						count: 32,
-						display_name: "S",
-						value: "S",
-						data: {},
-					},
-					{
-						status: "",
-						count: 3,
-						display_name: "XXL",
-						value: "XXL",
-						data: {},
-					},
-					{
-						status: "",
-						count: 2,
-						display_name: "XS",
-						value: "XS",
-						data: {},
-					},
-				],
-				hidden: false,
-				data: {},
-			},
-		],
-	},
 });
--- src/types/tests/search.test-d.ts
+++ src/types/tests/search.test-d.ts
@@ -1,12 +1,11 @@
 import { SearchResponse } from "../search";
 import { expectType } from "tsd";
 
-// Example response from https://www.princessauto.com/en/searchresults?Nr=product.active%3A1&Ntt=red*&Nty=1&No=0&Nrpp=50&Rdm=93&searchType=simple&type=search
 expectType<SearchResponse>({
 	response: {
 		result_sources: {
 			token_match: {
-				count: 454,
+				count: 122,
 			},
 			embeddings_match: {
 				count: 0,
@@ -32,8 +31,8 @@ expectType<SearchResponse>({
 				data: {},
 				children: [
 					{
-						group_id: "310-000-000-000",
-						display_name: "Trailer",
+						group_id: "123",
+						display_name: "123",
 						count: 62,
 						data: {},
 						children: [],
@@ -52,37 +51,21 @@ expectType<SearchResponse>({
 			{
 				matched_terms: ["red"],
 				data: {
-					id: "PA0009015918",
-					url: "https://www.princessauto.com/en/27-ton-212cc-gas-powered-log-splitter/product/PA0009015918",
+					id: "123",
+					url: "https://example",
 					keywords: ["Red Rock"],
-					image_url:
-						"https://www.princessauto.com/ccstore/v1/images/?source=/file/products/9015918_A0CG_00_01.jpg",
-					group_ids: ["220039", "420-010-015-010"],
-					deactivated: false,
-					description:
-						"Use this log splitter to split logs up to 26 in. long. It features horizontal and vertical operation, low-oil shutdown, auto cylinder return, 14-second cycle time and powder-coated steel construction.",
-					paProductStatus: "A",
-					paProductClearance: false,
-					listPrice: 1799.99,
-					SaleDates: "Oct 10 2022 - Oct 23 2022",
-					salePrice: 1399.99,
-					variation_id: "9015918",
-					selectorImage: "9015918_A0CG_00_01.jpg",
+					image_url: "https://example",
+					group_ids: ["123", "1234"],
 				},
-				value: "27 Ton 212cc Gas-Powered Log Splitter",
+				value: "123",
 				is_slotted: false,
 				labels: {},
 				variations: [
 					{
 						data: {
-							listPrice: 1799.99,
-							SaleDates: "Oct 10 2022 - Oct 23 2022",
-							salePrice: 1399.99,
-							deactivated: false,
-							variation_id: "9015918",
-							selectorImage: "9015918_A0CG_00_01.jpg",
+							variation_id: "123",
 						},
-						value: "27 Ton 212cc Gas-Powered Log Splitter",
+						value: "123",
 					},
 				],
 			},
@@ -137,250 +120,5 @@ expectType<SearchResponse>({
 			filter_items: "filter_items_w_atcs_and_purchases",
 		},
 		searchandized_items: {},
-	},
-});
-
-// Example response from https://bonobos.com/search?term=red
-expectType<SearchResponse>({
-	response: {
-		result_sources: {
-			token_match: {
-				count: 144,
-			},
-			embeddings_match: {
-				count: 0,
-			},
-		},
-		facets: [
-			{
-				display_name: "Accessory Size",
-				name: "Accessory Size",
-				type: "multiple",
-				options: [
-					{
-						status: "",
-						count: 4,
-						display_name: "OS",
-						value: "OS",
-						data: {},
-					},
-				],
-				hidden: false,
-				data: {},
-			},
-		],
-		groups: [
-			{
-				group_id: "this-is-a-fake-group_as_recommended-by-constructor",
-				display_name: "All",
-				count: 141,
-				data: {},
-				children: [
-					{
-						group_id: "19",
-						display_name: "Accessories",
-						count: 4,
-						data: {},
-						children: [],
-						parents: [
-							{
-								display_name: "All",
-								group_id: "this-is-a-fake-group_as_recommended-by-constructor",
-							},
-						],
-					},
-				],
-				parents: [],
-			},
-		],
-		results: [
-			{
-				matched_terms: ["red"],
-				data: {
-					id: "CSHRT00586-red molberg plaid",
-					url: "stretch-washed-button-down-shirt",
-					stores: ["Bonobos"],
-					swatches: [
-						{
-							on_sale: false,
-							markdown: false,
-							inventory: 861,
-							color_name: "red molberg plaid",
-							final_sale: false,
-							full_price: 89,
-							purchasable: true,
-							current_price: 89,
-							novelty_badge: "",
-							urgency_badge: "",
-							hover_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/273228/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SG2476_6_hover.jpg?1663699860=",
-							swatch_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/269951/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SG2476_swatch.jpg?1663340577=",
-							primary_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/273225/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SG2476_2_category.jpg?1663699840=",
-							color_presentation: "Red Molberg Plaid",
-						},
-					],
-					image_url:
-						"https://bonobos-prod-s3.imgix.net/products/273225/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SG2476_2_category.jpg?1663699840=",
-					is_bundle: false,
-					full_price: 89,
-					final_sale: false,
-					color_name: "red molberg plaid",
-					description:
-						"A soft cotton button-down updated with added stretch for flexing literally & figuratively.",
-					is_gift_card: false,
-					is_new_color: true,
-					current_price: 89,
-					is_new_program: false,
-					number_of_fits: 4,
-					hover_image_url:
-						"https://bonobos-prod-s3.imgix.net/products/273228/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SG2476_6_hover.jpg?1663699860=",
-					markdown_swatches: [
-						{
-							on_sale: true,
-							markdown: true,
-							inventory: 17,
-							color_name: "plum check",
-							final_sale: false,
-							full_price: 88,
-							purchasable: true,
-							current_price: 48,
-							novelty_badge: "",
-							urgency_badge: "",
-							hover_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/202675/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_26925-MVG62_3_hover.jpg?1603150771=",
-							swatch_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/195243/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_26925-MVG62_swatch.jpg?1600202642=",
-							primary_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/202679/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_26925-MVG62_40_category-outfitter.jpg?1603150785=",
-							color_presentation: "Plum Check",
-						},
-					],
-					number_of_swatches: 56,
-					full_price_swatches: [
-						{
-							on_sale: false,
-							markdown: false,
-							inventory: 861,
-							color_name: "red molberg plaid",
-							final_sale: false,
-							full_price: 89,
-							purchasable: true,
-							current_price: 89,
-							novelty_badge: "",
-							urgency_badge: "",
-							hover_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/273228/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SG2476_6_hover.jpg?1663699860=",
-							swatch_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/269951/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SG2476_swatch.jpg?1663340577=",
-							primary_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/273225/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SG2476_2_category.jpg?1663699840=",
-							color_presentation: "Red Molberg Plaid",
-						},
-					],
-					final_sale_swatches: [
-						{
-							on_sale: true,
-							markdown: false,
-							inventory: 1,
-							color_name: "heather blue plaid",
-							final_sale: true,
-							full_price: 89,
-							purchasable: true,
-							current_price: 39,
-							novelty_badge: "",
-							urgency_badge: "Only a few left",
-							hover_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/224814/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SB1105_2_hover.jpg?1632924864=",
-							swatch_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/223161/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SB1105_swatch.jpg?1632763858=",
-							primary_image_url:
-								"https://bonobos-prod-s3.imgix.net/products/224815/original/WOVEN-CASUAL-SHIRT_BUTTON-DOWN-WOVEN-SHIRT_BSH01760SB1105_3_category.jpg?1632924869=",
-							color_presentation: "Heather Blue Plaid",
-						},
-					],
-					number_of_markdown_swatches: 8,
-					number_of_full_price_swatches: 19,
-					number_of_final_sale_swatches: 29,
-					variation_id: "BSH01760DG2476LS",
-				},
-				value: "Stretch Everyday Shirt",
-				is_slotted: false,
-				labels: {},
-			},
-		],
-		sort_options: [
-			{
-				sort_by: "relevance",
-				display_name: "Featured",
-				sort_order: "descending",
-				status: "selected",
-			},
-		],
-		refined_content: [],
-		total_num_results: 144,
-		features: [
-			{
-				feature_name: "auto_generated_refined_query_rules",
-				display_name: "Affinity Engine",
-				enabled: true,
-				variant: {
-					name: "default_rules",
-					display_name: "Default weights",
-				},
-			},
-		],
-	},
-	result_id: "e0cd67c5-fbd0-4550-a1b7-fdbca97d03b8",
-	request: {
-		us: ["ecomm"],
-		num_results_per_page: 90,
-		blacklist_rules: true,
-		term: "red",
-		page: 1,
-		fmt_options: {
-			groups_start: "current",
-			groups_max_depth: 1,
-		},
-		sort_by: "relevance",
-		sort_order: "descending",
-		section: "Products",
-		features: {
-			query_items: true,
-			auto_generated_refined_query_rules: true,
-			manual_searchandizing: true,
-			personalization: true,
-			filter_items: true,
-		},
-		feature_variants: {
-			query_items: "query_items_ctr_l2r_ctr_ss",
-			auto_generated_refined_query_rules: "default_rules",
-			manual_searchandizing: null,
-			personalization: "default_personalization",
-			filter_items: "filter_items_w_atcs_and_purchases",
-		},
-		searchandized_items: {},
-	},
-});
-
-expectType<SearchResponse>({
-	response: {
-		redirect: {
-			data: {
-        url: "<url>"
-      },
-      matched_terms: [
-        "<a term>",
-        "<another term>",
-      ],
-      matched_user_segments: [
-        "<a segment>",
-        "<another segment>",
-      ]
-		},
-	},
-	result_id: "e0cd67c5-fbd0-4550-a1b7-fdbca97d03b8",
-	request: {
 	},
 });
