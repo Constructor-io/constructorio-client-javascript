1761f1b5f126e19654a48e5b1107021235531ddb
Jimmy Li <jimmyli921@gmail.com>
2023-09-13 16:04:03 -0700

    Add version to events file

--- src/utils/events.js
+++ src/utils/events.js
@@ -19,6 +19,8 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+// Version 3.3.0
+
 /* eslint-disable */
 
 'use strict';
