5eab0640ad6e4c99012407b5b82045fccce48e61
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2023-11-15 16:34:40 -0500

    Remove store2

--- src/utils/store_old.js
+++
@@ -1,8 +0,0 @@
-const store = require('store2');
-const overflow = require('./store.overflow');
-
-// Inject overflow into store
-// https://raw.githubusercontent.com/nbubna/store/master/src/store.overflow.js
-overflow(store, store._);
-
-module.exports = store;
