e71238ed574bcacbd5d7efac02050afd0a25d8c1
Chris Gee <christopherrgee@gmail.com>
2024-09-25 14:12:13 -0700

    Add analytic tags to trackSearchSubmitV2

--- src/types/tracker.d.ts
+++ src/types/tracker.d.ts
@@ -58,6 +58,19 @@ declare class Tracker {
     networkParameters?: NetworkParameters
   ): true | Error;
 
+  trackSearchSubmitV2(
+    term: string,
+    parameters: {
+      originalQuery: string;
+      groupId?: string;
+      displayName?: string;
+      section?: string;
+      userInput: string;
+      analyticsTags?: Record<string, string>;
+    },
+    networkParameters?: NetworkParameters
+  ): true | Error;
+
   trackSearchResultsLoadedV2(
     term: string,
     parameters: {
