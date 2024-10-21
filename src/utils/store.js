bb3b6608ee2075dff828910780c0b7d4de31e97d
Stanley Peng <speng1011@gmail.com>
2024-10-21 14:04:29 -0700

    remove unneeded logic

--- src/utils/store.js
+++ src/utils/store.js
@@ -27,7 +27,7 @@ const session = {
       return valueFromOverflow;
     }
 
-    if (!canUseStorage('sessionStorage') || typeof sessionStorage === 'undefined') {
+    if (!canUseStorage('sessionStorage')) {
       return null;
     }
 
@@ -57,12 +57,12 @@ const session = {
       delete this.overflow[key];
     }
 
-    if (canUseStorage('sessionStorage') || typeof sessionStorage !== 'undefined') {
+    if (canUseStorage('sessionStorage')) {
       sessionStorage.removeItem(key);
     }
   },
   key(i) {
-    if (!canUseStorage('sessionStorage') || typeof sessionStorage === 'undefined') {
+    if (!canUseStorage('sessionStorage')) {
       return Object.keys(this.overflow)?.[i];
     }
 
@@ -80,7 +80,7 @@ const session = {
   length() {
     const overflowLength = Object.keys(this.overflow).length;
 
-    if (!canUseStorage('sessionStorage') || typeof sessionStorage === 'undefined') {
+    if (!canUseStorage('sessionStorage')) {
       return overflowLength;
     }
 
@@ -89,7 +89,7 @@ const session = {
   clear() {
     this.overflow = {};
 
-    if (canUseStorage('sessionStorage') || typeof sessionStorage !== 'undefined') {
+    if (canUseStorage('sessionStorage')) {
       sessionStorage.clear();
     }
   },
@@ -105,7 +105,7 @@ const local = {
       return valueFromOverflow;
     }
 
-    if (!canUseStorage('localStorage') || typeof localStorage === 'undefined') {
+    if (!canUseStorage('localStorage')) {
       return null;
     }
     const valueFromLocal = localStorage.getItem(key);
@@ -134,12 +134,12 @@ const local = {
       delete this.overflow[key];
     }
 
-    if (canUseStorage('localStorage') || typeof localStorage !== 'undefined') {
+    if (canUseStorage('localStorage')) {
       localStorage.removeItem(key);
     }
   },
   key(i) {
-    if (!canUseStorage('localStorage') || typeof localStorage === 'undefined') {
+    if (!canUseStorage('localStorage')) {
       return Object.keys(this.overflow)?.[i];
     }
 
@@ -157,7 +157,7 @@ const local = {
   length() {
     const overflowLength = Object.keys(this.overflow).length;
 
-    if (!canUseStorage('localStorage') || typeof localStorage === 'undefined') {
+    if (!canUseStorage('localStorage')) {
       return overflowLength;
     }
 
@@ -166,7 +166,7 @@ const local = {
   clear() {
     this.overflow = {};
 
-    if (canUseStorage('localStorage') || typeof localStorage !== 'undefined') {
+    if (canUseStorage('localStorage')) {
       localStorage.clear();
     }
   },
