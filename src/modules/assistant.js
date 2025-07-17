e981519842e29686045064a2a49b328dcabc11f2
Evan Yan <evan.yan@constructor.io>
2025-07-16 20:42:31 -0700

    Retain assistant method doc

--- src/modules/assistant.js
+++ src/modules/assistant.js
@@ -13,7 +13,22 @@ class Assistant extends Agent {
   EventTypes = Agent.EventTypes;
 
   /**
-   * @deprecated Use getAgentResultsStream from the Agent module instead.
+   * Retrieve assistant results from EventStream
+   *
+   * @deprecated Use getAssistantResultsStream from the Agent module instead.
+   * @function getAssistantResultsStream
+   * @description Retrieve a stream of assistant results from Constructor.io API
+   * @param {string} intent - Intent to use to perform an intent based recommendations
+   * @param {object} [parameters] - Additional parameters to refine result set
+   * @param {string} [parameters.domain] - domain name e.g. swimming sports gear, groceries
+   * @param {number} [parameters.numResultsPerPage] - The total number of results to return
+   * @returns {ReadableStream} Returns a ReadableStream.
+   * @example
+   * const readableStream = constructorio.agent.getAssistantResultsStream('I want to get shoes', {
+   *     domain: "nike_sportswear",
+   * });
+   * const reader = readableStream.getReader();
+   * const { value, done } = await reader.read();
    */
   getAssistantResultsStream(query, parameters) {
     return this.getAgentResultsStream(query, parameters);
