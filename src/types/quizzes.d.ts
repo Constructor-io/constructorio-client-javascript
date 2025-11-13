d8037ffdf2af8862302f621e7b4fde7d640ebcc0
Vlad Saitov <vladislav.saitov@constructor.io>
2025-11-13 15:24:19 +0500

    [NO-REF] - introduce skip tracking param for quiz next question

--- src/types/quizzes.d.ts
+++ src/types/quizzes.d.ts
@@ -19,6 +19,7 @@ export interface QuizzesParameters {
   answers?: any[];
   quizVersionId?: string;
   quizSessionId?: string;
+  skipTracking?: boolean;
 }
 
 export interface QuizResultsFmtOptions {
