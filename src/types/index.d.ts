171360cb5a7359a9a17ed0f6d4012df6a8eb0eda
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2025-11-17 16:55:31 +0100

    Update src/types/index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -159,8 +159,8 @@ export interface FmtOptions extends Record<string, any> {
   groups_max_depth?: number;
   groups_start?: 'current' | 'top' | `group_id:${string}`;
   fields?: string[]; // Array of metadata field to be returned in the data response
-  hidden_fields?: string[],
-  show_hidden_fields?: boolean,
+  hidden_fields?: string[];
+  show_hidden_fields?: boolean;
   variations_return_type?: 'default' | 'all' | 'matched';
 }
 
